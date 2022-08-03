434a0576b7309f9533fc3bd7f28bf06606643b92
kira kira <kira@gmail.com>
2022-08-03 11:14:33 +0400

    Adding a new functional task

--- src/TodoApp/TodoList.js
+++ src/TodoApp/TodoList.js
@@ -1,49 +1,77 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../index.css";
 
 const ItemsListTodo = [
   {
-    id: 1,
-    title: "lorem 1 ",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "lorem 1",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "lorem 1",
-    completed: false,
-  },
-  {
-    id: 4,
-    title: "lorem 1",
+    id: "18372920",
+    title: "Add New Task 👇🏼",
     completed: false,
   },
 ];
+
 const TodoList = () => {
+  const [todos, setTodos] = useState(ItemsListTodo);
+  const [taskValue, setTaskValue] = useState("");
+  const taskInputRef = useRef()
 
- function onItemChange(item) {
-  console.log(item)
- }
+  useEffect(() => {
+    taskInputRef.current.focus();
+  }, [])
 
+
+  function onItemChange(itemClicked) {
+    const newListItem = todos.map((item) => {
+      if (item.id === itemClicked.id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setTodos(newListItem);
+  }
+
+  function onAddNewTask(e) {
+    e.preventDefault();
+    const newTask = [
+      ...todos,
+      {
+        id: Date.now(),
+        title: taskValue,
+        completed: false,
+      },
+    ];
+    setTodos(newTask);
+    setTaskValue('')
+  }
 
   return (
     <>
+    {/* Task container section */}
+      <div className="task_container">
+        <form action="" onSubmit={onAddNewTask}>
+          <input
+            className="task_input"
+            type="text"
+            maxLength="35"
+            placeholder="Add Task ..."
+            value={taskValue}
+            onChange={(e) => setTaskValue(e.target.value)}
+            ref={taskInputRef}
+          />
+        </form>
+      </div>
+      {/* New added task section container */}
       <div className="container">
         <ul>
-          {ItemsListTodo.map((item) => (
+          {todos.map((item) => (
             <div className="box">
+              <input
+                className="form_checkbox"
+                type="checkbox"
+                checked={item.completed}
+                onChange={() => onItemChange(item)}
+              />
 
-              <input 
-              className="form_checkbox"
-              type="checkbox"
-              checked={item.completed}
-              onChange={() => onItemChange(item)} />
-
-              <li key={item.id}  className={item.completed ? "completed" : ""}>
+              <li key={item.id} className={item.completed ? "completed" : ""}>
                 <div className="list_content ">{item.title}</div>
               </li>
 
